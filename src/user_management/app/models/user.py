21c623c34a15f4c837ddcaafbc3578a0dfd94d36
Edmilson Monteiro Rodrigues Neto <edmilson.rodriguesn38@gmail.com>
2025-04-16 10:06:11 +0100

    feat: add base SQLAlchemy integration

---
+++ src/user_management/app/models/user.py
@@ -0,0 +1,9 @@
+from app.sessions import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    full_name = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
